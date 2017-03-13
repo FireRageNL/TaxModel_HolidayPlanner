959602eff8e5e9eeaa80b4d8bfa54899948dad5d
Roy van Oldenbeek <r.vanoldenbeek@student.fontys.nl>
2017-03-13 15:26:36 +0100

    Added LoginModel unit tests
    and also removed one unused import

---
+++ src/test/java/HolidayPlanner/LoginTest.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HolidayPlanner;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author roy_v
+ */
+public class LoginTest {
+
+    public LoginTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getUserName method, of class Login.
+     */
+    @Test
+    public void testGetUserName() {
+        System.out.println("getUserName");
+        Login test = new Login();
+        test.setPassWord("aaaa");
+        test.setUserName("user");
+        Login instance = new Login(test);
+        String expResult = "user";
+        String result = instance.getUserName();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setUserName method, of class Login.
+     */
+    @Test
+    public void testSetUserName() {
+        System.out.println("setUserName");
+        String userName = "test";
+        Login instance = new Login();
+        instance.setUserName(userName);
+        assertEquals("test", instance.getUserName());
+    }
+
+    /**
+     * Test of getPassWord method, of class Login.
+     */
+    @Test
+    public void testGetPassWord() {
+        System.out.println("getPassWord");
+        Login test = new Login();
+        test.setPassWord("aaaa");
+        test.setUserName("user");
+        Login instance = new Login(test);
+        String expResult = "aaaa";
+        String result = instance.getPassWord();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setPassWord method, of class Login.
+     */
+    @Test
+    public void testSetPassWord() {
+        System.out.println("setPassWord");
+        String passWord = "bbbb";
+        Login instance = new Login();
+        instance.setPassWord(passWord);
+        assertEquals("bbbb",instance.getPassWord());
+    }
+
+}
