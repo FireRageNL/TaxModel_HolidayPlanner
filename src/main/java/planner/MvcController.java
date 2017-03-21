d836acf9ee11005f3edd6ac1a20d6bc0cba7ebf3
Roy van Oldenbeek <r.vanoldenbeek@student.fontys.nl>
2017-03-21 12:58:37 +0100

    edit to get 0(!) code smells
    I just couldnt stand the 1 code smell i needed to fix this

--- src/main/java/planner/MvcController.java
+++ src/main/java/planner/MvcController.java
@@ -1,7 +1,6 @@
 package planner;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
